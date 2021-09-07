627be2a64d8cc6c5fa4ca27a8bf8f7269d377b7d
Garrett Harty <gharty@live.com>
2021-09-07 16:14:14 -0500

    setup grid in StockDisplay component

--- src/components/StockDisplay/StockDisplay.jsx
+++ src/components/StockDisplay/StockDisplay.jsx
@@ -1,15 +1,28 @@
 import React from "react"
-import { Typography, List, ListItem, makeStyles } from "@material-ui/core"
+import { Typography, List, ListItem, makeStyles, Grid } from "@material-ui/core"
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import Accordion from '@material-ui/core/Accordion'
 import AccordionSummary from '@material-ui/core/AccordionSummary';
 import AccordionDetails from '@material-ui/core/AccordionDetails';
 import { useSelector } from "react-redux";
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      height: 140,
+      width: 100,
+    },
+    control: {
+      padding: theme.spacing(2),
+    },
+}));
+
 export default function StockDisplay({ stockSymbol, classes }) {
     const stock = useSelector(store => store.search);
     console.log(stock);
-    
+    const gridClass = useStyles();
 
     return (
         <React.Fragment>
@@ -26,20 +39,31 @@ export default function StockDisplay({ stockSymbol, classes }) {
                     </Typography>
                 </AccordionSummary>
                 <AccordionDetails>
-                    <List component="nav" aria-label="expanded stock info">
-                        <ListItem>
-                            High Price of the Day: ${stock.h}
-                        </ListItem>
-                        <ListItem>
-                            Low Price of the Day: ${stock.l}
-                        </ListItem>
-                        <ListItem>
-                            Opening Price: ${stock.o}
-                        </ListItem>
-                        <ListItem>
-                            Previous Day Closing Price: ${stock.pc}
-                        </ListItem>
-                    </List>
+                    <Grid container className={gridClass.root} spacing={2}>
+                        <Grid item xs={6}>
+                            <Grid container justifyContent="left" spacing={2}>
+                                <List component="nav" aria-label="expanded stock info">
+                                    <ListItem>
+                                        High Price of the Day: ${stock.h}
+                                    </ListItem>
+                                    <ListItem>
+                                        Low Price of the Day: ${stock.l}
+                                    </ListItem>
+                                    <ListItem>
+                                        Opening Price: ${stock.o}
+                                    </ListItem>
+                                    <ListItem>
+                                        Previous Day Closing Price: ${stock.pc}
+                                    </ListItem>
+                                </List>
+                            </Grid>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <Grid container spacing={2}>
+                                <p>graph</p>
+                            </Grid>
+                        </Grid>
+                    </Grid>
                 </AccordionDetails>
             </Accordion>
         </React.Fragment>
